7f595a206e5ddd7f93f4c38b2a47a73fc48ae0a6
Talina <talina@MacBook-Pro-Talina.local>
2022-05-22 11:29:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,187 @@
+/* eslint-disable no-magic-numbers */
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    let container = document.querySelector('.container');
+
+    function createTile() {
+        for (let i = 0; i < 9; i++) {
+            let div = document.createElement('div');
+            div.className = 'tile';
+            container.appendChild(div);
+        }
+    }
+    createTile();
+
+    const gragNDrop = () => {
+
+        let avatarContainer = document.querySelectorAll('.avatar-container');
+
+        let icon = document.querySelectorAll('.avatar-icon');
+
+        icon.forEach(draggable => {
+            draggable.addEventListener('dragstart', () => {
+                console.log('dragstart');
+                draggable.classList.add('dragging');
+            });
+        });
+
+        avatarContainer.forEach(container => {
+            container.addEventListener('dragover', () => {
+                const dragging = document.querySelector('.dragging');
+                if (container.children.length) {
+                    return;
+                }
+                container.appendChild(dragging);
+            });
+        });
+
+        icon.forEach(user => {
+            user.setAttribute('draggable', 'true');
+        });
+    };
+
+    gragNDrop();
+
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+        if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+        if (!board.includes('')) {
+            announce(TIE);
+        }
+    }
+
+    const announce = (type) => {
+        // eslint-disable-next-line default-case
+        switch(type) {
+            case PLAYERO_WON:
+                announcer.innerHTML = `Player <span class="playerO">O</span> Won`;
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = `Player <span class="playerX">X</span> Won`;
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
+        return true;
+    };
+
+    const updateBoard = (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+
+    const resetGame = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+
+    let start
+    [...document.getElementsByClassName('tile')].forEach(_ => {
+        _.addEventListener('click', () => {
+        dotheneedful(_)
+        })
+    })
+
+    function dotheneedful(sibling) {
+        if (sibling !== null) {
+            if (start){
+                start.focus();
+                start.style.backgroundColor = '';
+                start.style.color = '';
+            }
+            sibling.focus();
+            sibling.style.backgroundColor = '#a09d9d';
+            start = sibling;
+        }
+    }
+    
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'ArrowRight') {
+            dotheneedful(start.nextElementSibling);
+        } else if (e.key === 'ArrowLeft') {
+            dotheneedful(start.previousElementSibling);
+        } else if (e.key === 'Enter') {
+            start.click();
+        }
+    });
+
+    resetButton.addEventListener('click', resetGame);
+});
